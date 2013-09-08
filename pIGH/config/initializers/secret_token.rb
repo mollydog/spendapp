4d6e57c2f06c2eb78b13488c3974ebb63a49e753
Jen Lessor <jen_lessor@yahoo.com>
2013-09-07 22:00:20 -0400

    Initial commit

--- pIGH/config/initializers/secret_token.rb
+++ pIGH/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PIGH::Application.config.secret_token = '0b819e99b1e3ac96831218ddf63af3ef0fe4cfe51609e4862a6067f037d90d94f8c33e7245d50a1dd6a9dc63c88e1c045aa26f9eed2a48f43f48c8f908fafc43'
+PIGH::Application.config.secret_token = 'e00e379a03910b88725e0e52cd20929d445cf6db7c272480e0cffaef20f70420f654607cebf87a1a8efc6cb0c2708c458f5f1d19d7976286adf7fbc39d875281'
